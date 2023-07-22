a806e30423dd1cc7fd735cefeb0d9520056d6cfe
Jacob Gunther <jacobgunther03+github@gmail.com>
2023-07-22 03:04:52 -0500

    Fix bug

--- src/app/player/[id]/icon.js
+++ src/app/player/[id]/icon.js
@@ -16,7 +16,11 @@ export default async function Icon({ params: { id } }) {
     return new ImageResponse(
         (
             <div style={{ display: 'flex' }}>
-                <img src={`${process.env.NEXT_PUBLIC_API_HOST}/head/${profile.id}?scale=4`} width="55" height="64" style={{ left: '4px' }} />
+                {
+                    profile
+                        ? <img src={`${process.env.NEXT_PUBLIC_API_HOST}/head/${profile.id}?scale=4`} width="55" height="64" style={{ left: '4px' }} />
+                        : <img src="https://mineatar.io/img/icon.png" width="64" height="64" />
+                }
             </div>
         ),
         size
