1f310ecfa166893315429a155c9975ba8a5e1e9d
Jacob Gunther <jacobgunther03+github@gmail.com>
2023-08-27 21:21:42 -0500

    Fix duplicate navbar

--- src/app/player/[id]/layout.js
+++ src/app/player/[id]/layout.js
@@ -1,14 +1,10 @@
 import HeroSearch from '@/components/HeroSearch';
-import Navbar from '@/components/Navbar';
 
 export default async function Page({ params: { id }, children }) {
     return (
         <>
-            <Navbar />
-            <div className="container py-12">
-                <HeroSearch defaultUsername={id} />
-                {children}
-            </div>
+            <HeroSearch defaultUsername={id} />
+            {children}
         </>
     );
 }
