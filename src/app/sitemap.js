6e26d0db5f411d1a0a55548d62cb44bf69519b29
Jacob Gunther <jacobgunther03+github@gmail.com>
2024-02-24 23:35:19 -0600

    Reduce sitemap length

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,16 +1,9 @@
 export default async function generateSitemap() {
-    const result = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/list`, {
-        headers: {
-            Authorization: process.env.AUTH_KEY
-        }
-    });
-
-    const body = await result.json();
-
     const lastModified = new Date().toISOString();
 
     return [
         { url: 'https://mineatar.io/', lastModified },
-        ...body.map((uuid) => ({ url: `https://mineatar.io/player/${uuid}`, lastModified }))
+        { url: 'https://mineatar.io/docs', lastModified },
+        { url: 'https://mineatar.io/about', lastModified }
     ];
 }
